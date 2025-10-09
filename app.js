12e74c810a572ce186eeac59831ae199c1788f93
asmodeus78 <asmodeus78@gmail.com>
2025-10-09 21:41:50 +0000

    fix cors 3

--- app.js
+++ app.js
@@ -9,18 +9,7 @@ const ADMIN_API_VERSION = '2025-07';
 
 const app = express();
 app.use(cors());
-app.use(function (req,res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-    );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-});
+
 
 
 
